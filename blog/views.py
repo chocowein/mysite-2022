240a6ef77a334d52436feefdfabe8388db469153
chocowein <chokoweinochoko@gmail.com>
2022-10-28 10:58:32 +0900

    ユーザー登録

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 # render 関数を import している
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
 
 # render 関数でテンプレートを表示するように
 def index(request):
@@ -10,3 +11,13 @@ def index(request):
 class AccountCreateView(View):
     def get(self, request):
         return render(request, "blog/register.html")
+
+    # post を追加
+    def post(self, request):
+    # ユーザー情報を保存する
+        User.objects.create_user(
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        # 登録完了画面を表示する
+        return render(request, "blog/register_success.html")
