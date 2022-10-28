155ce64930102261bc20eb298385fdd6bf61c776
chocowein <chokoweinochoko@gmail.com>
2022-10-28 10:47:23 +0900

    ユーザーとパスワード

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from blog import views
 urlpatterns = [
     # views.py の index() 関数を呼び出す
     path('', views.index, name='index'),
-    path('detail', views.detail, name='detail'),
+    path("register", views.AccountCreateView.as_view(), name="register"),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 # import を追加
 # render 関数を import している
 from django.shortcuts import render
+from django.views import View
 
 # render 関数でテンプレートを表示するように
 def index(request):
     return render(request, "blog/index.html")
+
+class AccountCreateView(View):
+    def get(self, request):
+        return render(request, "blog/register.html")
