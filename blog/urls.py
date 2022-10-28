88227bc8d6fc06b28b372d73e64d46b3729ab723
chocowein <chokoweinochoko@gmail.com>
2022-10-28 11:48:05 +0900

    ログインログアウトのあれこれ

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # views.py の index() 関数を呼び出す
     path('', views.index, name='index'),
     path("register", views.AccountCreateView.as_view(), name="register"),
+    path("login", views.AccountLoginView.as_view(), name="login"),
+    path("mypage", views.MypageView.as_view(), name="mypage"),
+    path("logout", views.AccountLogoutView.as_view(), name="logout"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 
 # render 関数でテンプレートを表示するように
 def index(request):
@@ -21,3 +24,25 @@ class AccountCreateView(View):
         )
         # 登録完了画面を表示する
         return render(request, "blog/register_success.html")
+
+class AccountLoginView(LoginView):
+    """ログインページのテンプレート"""
+    template_name = 'blog/login.html'
+
+    def get_default_redirect_url(self):
+        """ログインに成功した時に飛ばされるURL"""
+        return "/blog"
+
+class MypageView(View):
+    def get(self, request):
+        return render(request, "blog/mypage.html")
+
+class MypageView(LoginRequiredMixin, View):
+    # ログインしていない場合に飛ばすページの設定
+    login_url = '/blog/login'
+
+    def get(self, request):
+        return render(request, "blog/mypage.html")
+
+class AccountLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
