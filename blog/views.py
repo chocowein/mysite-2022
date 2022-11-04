8c492e2129843a294f436a1b856887b6e5c1d1c5
chocowein <chokoweinochoko@gmail.com>
2022-11-04 11:44:48 +0900

    記事のあれこれ

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from blog.models import Article
 
+
 # render 関数でテンプレートを表示するように
 def index(request):
     return render(request, "blog/index.html")
@@ -39,31 +40,16 @@ class MypageView(View):
         return render(request, "blog/mypage.html")
 
 class MypageView(LoginRequiredMixin, View):
-    # ログインしていない場合に飛ばすページの設定
     login_url = '/blog/login'
 
     def get(self, request):
-        return render(request, "blog/mypage.html")
+        articles = Article.objects.filter(user=request.user)
+        return render(request, "blog/mypage.html", {
+            "articles": articles
+        })
 
 class AccountLogoutView(LogoutView):
     template_name = 'blog/logout.html'
-
-class ArticleCreateView(LoginRequiredMixin, View):
-    def get(self, request):
-        return render(request, "blog/article_new.html")
-
-class MypageArticleView(LoginRequiredMixin, View):
-    def post(self, request):
-        """新しく記事を作製する"""
-        # リクエストで受け取った情報をDBに保存する
-        article = Article(
-            title=request.POST["title"],
-            body=request.POST["body"],
-            # user には、現在ログイン中のユーザーをセットする
-            user=request.user,
-        )
-        article.save()
-        return render(request, "blog/article_created.html")
 
 class ArticleListView(View):
     def get(self, request):
@@ -76,3 +62,47 @@ class ArticleListView(View):
         return render(request, "blog/articles.html", {
             "articles": articles
         })
+
+class ArticleCreateView(LoginRequiredMixin, View):
+    login_url = '/blog/login'
+
+    def get(self, request):
+        """記事を書く画面を表示するリクエスト"""
+        return render(request, "blog/article_new.html")
+
+class MypageArticleView(LoginRequiredMixin, View):
+    login_url = '/blog/login'
+    
+    def post(self, request):
+        """記事を保存する"""
+        # リクエストで受け取った情報をDBに保存する
+        article = Article(
+            title=request.POST["title"],
+            body=request.POST["body"],
+            # user には、現在ログイン中のユーザーをセットする
+            user=request.user,
+        )
+        article.save()
+        return render(request, "blog/article_created.html")
+
+def index(request):
+    # Article の model を使ってすべての記事を取得する
+    # Article.objects.all() は article のリストが返ってくる
+    articles = Article.objects.all()
+
+    # こうすることで、article 変数をテンプレートにわたす事ができる
+    # {テンプレート上での変数名: 渡す変数}
+    return render(request, "blog/index.html", {
+        "articles": articles
+    })
+
+    # 記事についての View
+class ArticleView(View):
+    # urls.py の <id> が、 id に入る
+    def get(self, request, id):
+        # get は条件に合致した記事を一つ取得する
+        article = Article.objects.get(id=id)
+        return render(request, "blog/article.html", {
+            "article": article,
+        })
+
